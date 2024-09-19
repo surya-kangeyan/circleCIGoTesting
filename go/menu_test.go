7dfcad1f3d35b6c358045b56f88817e29bd45bbf
surya-kangeyan <suryakangeyan4701@gmail.com>
2024-09-18 23:25:05 -0700

    included mock test

--- go/menu_test.go
+++ go/menu_test.go
@@ -57,3 +57,33 @@ func TestShowMenuItemByIdHandler(t *testing.T) {
 
 	assert.Equal(t, http.StatusNotFound, w.Code)
 }
+// package openapi
+
+// import (
+// 	"encoding/json"
+// 	"net/http"
+// 	"net/http/httptest"
+// 	"testing"
+
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/stretchr/testify/assert"
+// )
+
+// func SetUpRouter() *gin.Engine {
+// 	router := gin.Default()
+// 	return router
+// }
+
+// func TestGetMenuItemsHandler(t *testing.T) {
+// 	r := SetUpRouter()
+// 	r.GET("/testmenuitems", GetTestMenuItems)
+// 	req, _ := http.NewRequest("GET", "/testmenuitems", nil)
+// 	w := httptest.NewRecorder()
+// 	r.ServeHTTP(w, req)
+
+// 	var menuitems []MenuItem
+// 	json.Unmarshal(w.Body.Bytes(), &menuitems)
+
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// 	assert.NotEmpty(t, menuitems)
+// }
